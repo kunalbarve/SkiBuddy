598c97829711166384784378148ec06273965362
Barve, Kunal(knbarve) <knbarve@paypal.com>
2015-11-30 19:18:42 -0800

    Event Model Added

---
+++ app/src/main/java/com/cmpe277/skibuddy/Models/Event.java
@@ -0,0 +1,71 @@
+package com.cmpe277.skibuddy.Models;
+
+/**
+ * Created by knbarve on 11/30/15.
+ */
+public class Event {
+
+    private String id;
+    private String name;
+    private String description;
+    private String startDate;
+    private String startTime;
+    private String endDate;
+    private String endTime;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+}
