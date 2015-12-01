5fed11d469645a6c02217cf80c55c1a66cd4a2f3
Barve, Kunal(knbarve) <knbarve@paypal.com>
2015-11-30 19:37:19 -0800

    Models Updated
    Group model added

---
+++ app/src/main/java/com/cmpe277/skibuddy/Models/Group.java
@@ -0,0 +1,44 @@
+package com.cmpe277.skibuddy.Models;
+
+/**
+ * Created by knbarve on 11/30/15.
+ */
+public class Group {
+
+    private String id;
+    private String userId;
+    private String eventId;
+    private String status;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(String eventId) {
+        this.eventId = eventId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
